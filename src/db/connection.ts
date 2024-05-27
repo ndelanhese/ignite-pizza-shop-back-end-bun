d0e89dfc65eb29a1428e72a9872e618de95c8587
ndelanhese <nnathanh3@gmail.com>
2024-05-27 08:30:11 -0300

    feat(db-seed): add seeder

---
+++ src/db/connection.ts
@@ -0,0 +1,8 @@
+import { env } from "@env";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
+
+const connection = postgres(env.DB_URL);
+
+export const db = drizzle(connection, { schema });
