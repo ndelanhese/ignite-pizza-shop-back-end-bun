cbdeab865cc8122dbec28f0ac4c7d3800bb0796a
ndelanhese <nnathanh3@gmail.com>
2024-05-28 06:34:06 -0300

    feat(server): add jwt and cookies service

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
 	DB_URL: z.string().url().min(1),
 	API_BASE_URL: z.string().url().min(1),
 	AUTH_REDIRECT_URL: z.string().url().min(1),
+	JWT_SECRET_KEY: z.string(),
 	API_PORT: z.coerce.number().default(3333),
 });
 
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,24 @@
+import cookie from "@elysiajs/cookie";
+import jwt from "@elysiajs/jwt";
 import { env } from "@env";
 import chalk from "chalk";
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { registerRestaurant } from "./routes/register-restaurant";
 import { sendAuthLink } from "./routes/send-auth-link";
 
-const app = new Elysia().use(registerRestaurant).use(sendAuthLink);
+const app = new Elysia()
+	.use(
+		jwt({
+			secret: env.JWT_SECRET_KEY,
+			schema: t.Object({
+				sub: t.String(),
+				restaurantId: t.Optional(t.String()),
+			}),
+		}),
+	)
+	.use(cookie())
+	.use(registerRestaurant)
+	.use(sendAuthLink);
 
 app.listen(env.API_PORT, () => {
 	console.log(chalk.green("🚀 HTTP server running!"));
